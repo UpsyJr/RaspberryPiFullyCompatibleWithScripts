ffc77d80c2a59188dc569bbea964f6d5fd23ddb8
UpsyJr <47011269+UpsyJr@users.noreply.github.com>
2019-07-02 12:39:50 +0300

    Create ausintuvasOn.py

---
+++ ausintuvasOn.py
@@ -0,0 +1,8 @@
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(22, GPIO.OUT) # this is my set gpio from the raspberry pi, you replace it with our own!!!
+
+GPIO.output(22, True)
